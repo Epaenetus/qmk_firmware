c18f92cbdbb8c9cafc6d1ac2c10ae855f2098e15
k.chargualaf <k.chargualaf@digipen.edu>
2018-07-10 07:45:40 -0700

    Remap version 2

--- keyboards/handwired/dactyl/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl/keymaps/default/keymap.c
@@ -35,22 +35,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_dactyl(  // layer 0 : default
         // left hand
            KC_EQL,         KC_1,           KC_2,     KC_3,     KC_4,  KC_5,
-          KC_DELT,         KC_Q,           KC_W,     KC_E,     KC_R,  KC_T,
-          KC_BSPC,         KC_A,           KC_S,     KC_D,     KC_F,  KC_G,
-          KC_LSFT,  CTL_T(KC_Z),           KC_X,     KC_C,     KC_V,  KC_B,
-  LT(SYMB,KC_GRV),      KC_QUOT,  LALT(KC_LSFT),  KC_LEFT,  KC_RGHT,
+           KC_ESC,         KC_Q,           KC_W,     KC_F,     KC_P,  KC_B,
+           KC_TAB,         KC_A,           KC_R,     KC_S,     KC_T,  KC_G,
+          KC_LSFT,  CTL_T(KC_Z),           KC_X,     KC_C,     KC_D,  KC_V,
+  				KC_LGUI,      KC_QUOT,  			KC_LALT,  	KC_UP,  KC_DOWN,
                                                                       ALT_T(KC_APP),  KC_LGUI,
                                                                                       KC_HOME,
-                                                             KC_SPC,        KC_BSPC,   KC_END,
-        // right hand
-                             KC_6,   KC_7,     KC_8,     KC_9,               KC_0,         KC_MINS,
-                             KC_Y,   KC_U,     KC_I,     KC_O,               KC_P,         KC_BSLS,
-                             KC_H,   KC_J,     KC_K,     KC_L,  LT(MDIA, KC_SCLN),  GUI_T(KC_QUOT),
-                             KC_N,   KC_M,  KC_COMM,   KC_DOT,     CTL_T(KC_SLSH),         KC_RSFT,
-                                    KC_UP,  KC_DOWN,  KC_LBRC,            KC_RBRC,          KC_FN1,
-          KC_RALT,  CTL_T(KC_ESC),
+                                                             MO(SYMB),        KC_SPC,   KC_END, // right hand
+                             KC_6,   KC_7,     KC_8,     			KC_9,               KC_0,         	KC_MINS,
+                             KC_J,   KC_L,     KC_U,     			KC_Y,            KC_SCLN,      		KC_CAPS,
+                             KC_M,   KC_N,     KC_E,     			KC_I,  						 KC_O,  			 	KC_BSPC,
+                             KC_K,   KC_H,  KC_COMM,   			KC_DOT,     CTL_T(KC_SLSH),         	KC_RSFT,
+                                  KC_LEFT, KC_RIGHT,ALT_T(KC_LBRC),       KC_RBRC,          KC_RGUI,
+          KC_PSCR,  CTL_T(KC_ESC),
           KC_PGUP,
-          KC_PGDN, KC_TAB, KC_ENT
+          KC_PGDN, KC_ENT, MO(SYMB)
     ),
 /* Keymap 1: Symbol Layer
  *
@@ -79,16 +78,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           VRSN,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,
        KC_TRNS,  KC_EXLM,    KC_AT,  KC_LCBR,  KC_RCBR,  KC_PIPE,
        KC_TRNS,  KC_HASH,   KC_DLR,  KC_LPRN,  KC_RPRN,   KC_GRV,
-       KC_TRNS,  KC_PERC,  KC_CIRC,  KC_LBRC,  KC_RBRC,  KC_TILD,
+       KC_LSFT,  KC_PERC,  KC_CIRC,  KC_LBRC,  KC_RBRC,  KC_TILD,
          RESET,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
                                                          KC_TRNS,  KC_TRNS,
                                                                    KC_TRNS,
                                                KC_TRNS,  KC_TRNS,  KC_TRNS,
        // right hand
                 KC_F6,      KC_F7,   KC_F8, KC_F9,  KC_F10,   KC_F11,
-                KC_UP,       KC_7,    KC_8,  KC_9, KC_ASTR,   KC_F12,
-                KC_DOWN,     KC_4,    KC_5,  KC_6, KC_PLUS,  KC_TRNS,
-                KC_AMPR,     KC_1,    KC_2,  KC_3, KC_BSLS,  KC_TRNS,
+                KC_F10,       KC_7,    KC_8,  KC_9, KC_ASTR,   KC_F12,
+                KC_F11,     KC_4,    KC_5,  KC_6, KC_PLUS,  KC_TRNS,
+                KC_AMPR,     KC_1,    KC_2,  KC_3, KC_BSLS,  KC_RSFT,
                           KC_TRNS,  KC_DOT,  KC_0,  KC_EQL,  KC_TRNS,
       KC_TRNS,  KC_TRNS,
       KC_TRNS,
